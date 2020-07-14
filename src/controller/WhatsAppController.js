d6202d0ce73815d673f1d270d33602f0376eb78f
frepilatti <frepilatti@hotmail.com>
2020-07-14 08:32:28 -0300

    Insert Emoji added, webpack and webpack-dev-server added, open camera and take picture using canvas, sending documents attached

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,4 +1,8 @@
-class WhatsAppController
+import {Format} from './../util/Format';
+import {CameraController} from './CameraController';
+import {DocumentPreviewController} from './DocumentPreviewController';
+
+export class WhatsAppController
 {
 
     constructor()
@@ -103,27 +107,35 @@ class WhatsAppController
     initEvents()
     {
         this.el.myPhoto.on('click', e=>{
+
             this.closeAllLeftPanel();
             this.el.panelEditProfile.show();
             setTimeout(()=>{
                 this.el.panelEditProfile.addClass('open');
             }, 100);
+
         });
 
         this.el.btnNewContact.on('click', e=>{
+
             this.closeAllLeftPanel();
             this.el.panelAddContact.show();
             setTimeout(()=>{
                 this.el.panelAddContact.addClass('open');
             }, 100);
+
         });
 
         this.el.btnClosePanelEditProfile.on('click',e=>{
+
             this.el.panelEditProfile.removeClass('open');
+
         });
 
         this.el.btnClosePanelAddContact.on('click', e=>{
+
             this.el.panelAddContact.removeClass('open');
+
         });
 
         this.el.photoContainerEditProfile.on('click', e=>{
@@ -131,89 +143,331 @@ class WhatsAppController
         });
 
         this.el.inputNamePanelEditProfile.on('keypress', e=>{
+
             if (e.key === 'Enter') {
                 e.preventDefault();
                 this.el.btnSavePanelEditProfile.click();
             }
+
         });
 
         this.el.btnSavePanelEditProfile.on('click', e=>{
+
             console.log(this.el.inputNamePanelEditProfile.innerHTML);
+
         });
 
         this.el.formPanelAddContact.on('submit', e=>{
+
             e.preventDefault();
             let formData = new FormData(this.el.formPanelAddContact);
+
         });
         
         this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item=>{
+
             item.on('click',e=>{
+
                 this.el.home.hide();
                 this.el.main.css({
                     display:'flex'
                 });
+
             });
+
         });
 
         this.el.btnAttach.on('click', e=>{
+
             e.stopPropagation();
             this.el.menuAttach.addClass('open');
             document.addEventListener('click', this.closeMenuAttach.bind(this), {once : true});
+
         });
 
         this.el.btnAttachPhoto.on('click', e=>{
+
             this.el.inputPhoto.click();
+
         });
 
         this.el.inputPhoto.on('change', e=>{
+
             console.log(this.el.inputPhoto.files);
             [...this.el.inputPhoto.files].forEach(file=>{
                 console.log(file);
             });
+
         });
 
         this.el.btnAttachCamera.on('click', e=>{
+
             this.closeAllMainPanel();
             this.el.panelCamera.addClass('open');
             this.el.panelCamera.css({
                 'height':'105%'
             });
+
+            this._camera = new CameraController(this.el.videoCamera);
+
         });
 
         this.el.btnClosePanelCamera.on('click',e=>{
+
             this.closeAllMainPanel();
             this.el.panelMessagesContainer.show();
+            this._camera.stop();
+
         });
 
         this.el.btnTakePicture.on('click', e=>{
-            console.log('take picture');
+
+            let dataUrl = this._camera.takePicture();
+
+            this.el.pictureCamera.src = dataUrl;
+            this.el.pictureCamera.show();
+            this.el.videoCamera.hide();
+            this.el.btnReshootPanelCamera.show();
+            this.el.containerTakePicture.hide();
+            this.el.containerSendPicture.show();
+
         });
+
+        this.el.btnReshootPanelCamera.on('click', e=>{
+
+            this.el.pictureCamera.hide();
+            this.el.videoCamera.show();
+            this.el.btnReshootPanelCamera.hide();
+            this.el.containerTakePicture.show();
+            this.el.containerSendPicture.hide();
+
+        });
+
+        
+        this.el.btnSendPicture.on('click', e=>{
+
+            console.log(this.el.pictureCamera.src);
+
+        });
+
         
         this.el.btnAttachDocument.on('click', e=>{
+
             this.closeAllMainPanel();
             this.el.panelDocumentPreview.addClass('open');
             this.el.panelDocumentPreview.css({
                 'height':'110%'
             });
+
+            this.el.inputDocument.click();
+
+        });
+
+        this.el.inputDocument.on('change', e=>{
+
+            if(this.el.inputDocument.files.length)
+            {
+
+                let file = this.el.inputDocument.files[0];
+
+                this._documentPreviewController = new DocumentPreviewController(file);
+
+
+                this._documentPreviewController.getPreviewData().then(result=>{
+
+                    this.el.imgPanelDocumentPreview.src = result.src;
+                    this.el.infoPanelDocumentPreview.innerHTML = result.info;
+                    this.el.imagePanelDocumentPreview.show();
+                    this.el.filePanelDocumentPreview.hide();
+
+                }).catch(err=>{
+
+                    console.log(file.type);
+
+                    switch (file.type) {
+                        
+                        case 'application/vnd.ms-excel':
+                        case 'application/vnd.openxmlformats-officedocument.spreadsheetmh.sheet':
+
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-xls';
+                        
+                        break;
+
+                        case 'application/vnd.ms-powerpoint':
+                        case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-ppt';
+
+                        break;
+
+                        case 'application/msword':
+                        case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-doc';
+
+                        break;
+
+                        default:
+
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-generic';
+
+                        break;
+
+                    }
+
+                    this.el.filenamePanelDocumentPreview.innerHTML = file.name;
+                    this.el.imagePanelDocumentPreview.hide();
+                    this.el.filePanelDocumentPreview.show();
+
+                });
+
+            }
+
         });
         
         this.el.btnClosePanelDocumentPreview.on('click', e=>{
+
             this.closeAllMainPanel();
             this.el.panelMessagesContainer.show();
-        })
+
+        });
 
         this.el.btnSendDocument.on('click',e=>{
+
             console.log('send document');
+
         });
         
         this.el.btnAttachContact.on('click', e=>{
+
             this.el.modalContacts.show();
+
         });
 
         this.el.btnCloseModalContacts.on('click',e=>{
+
             this.el.modalContacts.hide();
+
+        });
+
+        this.el.btnSendMicrophone.on('click', e=>{
+
+            this.el.recordMicrophone.show();
+            this.el.btnSendMicrophone.hide();
+            this.startRecordMicrophoneTime();
+
+        });
+
+        this.el.btnCancelMicrophone.on('click', e=>{
+
+            this.closeRecordMicrophone();
+
+        });
+
+        this.el.btnFinishMicrophone.on('click', e=>{
+
+            this.closeRecordMicrophone();
+
+        });
+
+        this.el.inputText.on('keyup', e=>{
+
+            if (this.el.inputText.innerHTML.length)
+            {
+                this.el.inputPlaceholder.hide();
+                this.el.btnSendMicrophone.hide();
+                this.el.btnSend.show();
+            } else {
+                this.el.inputPlaceholder.show();
+                this.el.btnSendMicrophone.show();
+                this.el.btnSend.hide();
+            }
+
+        });
+
+        this.el.inputText.on('keypress', e=>{
+            if (e.key === 'Enter' && !e.ctrlKey)
+            {
+                e.preventDefault();
+                this.el.btnSend.click();
+            }
         })
 
+        this.el.btnSend.on('click', e=>{
+
+            console.log(this.el.inputText.innerHTML);
+
+        });
+
+        this.el.btnEmojis.on('click', e=>{
+
+            this.el.panelEmojis.toggleClass('open');
+
+        });
+
+        this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji=>{
+
+            emoji.on('click', e=>{
+
+                let img = this.el.imgEmojiDefault.cloneNode();
+
+                img.style.cssText = emoji.style.cssText;
+                img.dataset.unicode = emoji.dataset.unicode;
+                img.alt = emoji.dataset.unicode;
+
+                emoji.classList.forEach(name=>{
+
+                    img.classList.add(name);
+
+                });
+
+                let cursor = window.getSelection();
+
+                if (!cursor.focusNode || !cursor.focusNode.id == 'input-text')
+                {
+                    this.el.inputText.focus();
+
+                    cursor = window.getSelection();
+                }
+
+                let range = document.createRange();
+
+                range = cursor.getRangeAt(0);
+
+                range.deleteContents();
+
+                let frag = document.createDocumentFragment();
+
+                frag.appendChild(img);
+
+                range.insertNode(frag);
+
+                range.setStartAfter(img);
+
+                this.el.inputText.dispatchEvent(new Event('keyup'));
+
+            });
+
+        });
+
+    }
+
+    startRecordMicrophoneTime()
+    {
+        let start = Date.now();
+
+        this._recordMicrophoneInterval = setInterval(()=>{
+
+            this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
+        
+        }, 100);
+    }
+
+    closeRecordMicrophone()
+    {
+        this.el.recordMicrophone.hide();
+        this.el.btnSendMicrophone.show();
+        clearInterval(this._recordMicrophoneInterval);
     }
 
     closeAllMainPanel()
--- src/util/Format.js
+++ src/util/Format.js
@@ -1,7 +1,6 @@
-class Format
+export class Format
 {
     static getCamelCase(text)
-    // Esse metodo cria automaticamente os atributos para cada uma das id dentro do HTML ja padronisados em CamelCase
     {
 
         let div = document.createElement('div');
@@ -10,4 +9,22 @@ class Format
 
         return Object.keys(div.firstChild.dataset)[0];
     }
+
+    static toTime(duration)
+    {
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+    
+        if (hours > 0)
+        {
+            return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        } 
+        else
+        {
+            return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+        }
+
+    }
+    
 }
